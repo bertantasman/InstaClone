39d4e546a667f27ea258c755bfe7622f626d6bc6
Ahmet Bertan Taşman <bertantasman@gmail.com>
2025-03-28 13:24:11 +0300

    Initial Commit

---
+++ InstagramKlon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstagramKlon
+//
+//  Created by Bertan Taşman on 28.03.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ InstagramKlonTests/InstagramKlonTests.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramKlonTests.swift
+//  InstagramKlonTests
+//
+//  Created by Bertan Taşman on 28.03.2025.
+//
+
+import Testing
+@testable import InstagramKlon
+
+struct InstagramKlonTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
